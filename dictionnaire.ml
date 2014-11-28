6323d1cb420261c559a106dc86b371689cc33305
Quego <quentinmace@hotmail.com>
2014-11-28 04:34:20 +0100

    Maj du module dictionnaire (fontion add)

--- dictionnaire.ml
+++ dictionnaire.ml
@@ -20,10 +20,21 @@ module Dictionnaire =
 		  in member (String.sub s 1 ((String.length s) -1)) db
 	  | Feuille -> false
 
-    (*    let (add : string -> dico -> int) = fun s d ->
-      let x = String.get s 0
-       in let y = (Char.code x) - (Char.code 'a')
-	  in Array.set d y (Noeud([|Feuille|], true))  *)
+    let rec (add : string -> dico -> dico) = fun s d ->
+      if ((String.length s) == 0 ) then 
+	match d with
+	  |(Noeud(da,b)) -> (Noeud(da,true))
+	  | Feuille -> Noeud((Array.make 26 Feuille), true)
+      else 
+	match d with
+	  | (Noeud(da,b)) ->
+	    let x = String.get s 0 
+	    in let y = (Char.code x) - (Char.code 'a')
+	       in let db =  (Array.get da y)
+		  in let z = add (String.sub s 1 ((String.length s) -1)) db
+		     in Array.set da y z;
+		     Noeud(da,b);
+	  | Feuille -> add (String.sub s 1 ((String.length s) -1)) dico_vide
 
 
   end
@@ -31,10 +42,18 @@ module Dictionnaire =
 
 String.sub "aaaa" 1 3;;
 
-
+Array.set a n x modifies array a in place, replacing element number n with x. You can also write a.(n) <- x instead of Array.set a n x. 
 let x = Dictionnaire.dico_vide;;
  
+Noeud((Array.set da y ( add (String.sub s 1 ((String.length s) -1)) db)),b)
 
+	    let x = String.get s 0 
+	    in let y = (Char.code x) - (Char.code 'a')
+	       in let db =  (Array.get da y)
+		  in let w = (add (String.sub s 1 ((String.length s) -1)) db)
+		     in let z = (Array.set da y w)
+			in Noeud(z,b)
+	  
 let x =    Dictionnaire.Noeud
    ([|Dictionnaire.Noeud
    ([|Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
@@ -57,4 +76,8 @@ let x =    Dictionnaire.Noeud
       Dictionnaire.Feuille; Dictionnaire.Feuille|],
    false);;
 
-Dictionnaire.member "a" x;; 
+Dictionnaire.member "aaa" x;; 
+
+Dictionnaire.add "b" x;;
+
+Dictionnaire.member "b" x;;
