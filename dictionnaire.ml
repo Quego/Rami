e41bb470e51fce7114a64a1ca9e6bfd1c2ed4cd8
Quego <quentinmace@hotmail.com>
2014-11-28 03:53:34 +0100

    Maj du module dictionnaire

--- dictionnaire.ml
+++ dictionnaire.ml
@@ -5,7 +5,20 @@ module Dictionnaire =
 
     let (dico_vide : dico ) = Noeud((Array.make 26 Feuille), false)
 
-   (* let (member : string -> dico -> bool ) *)
+(* Cas avec * a rajouter *)
+    let rec (member : string -> dico -> bool)  = fun s d ->
+      if ((String.length s) == 0 ) then 
+	match d with
+	  |(Noeud(_,b)) -> b 
+	  | Feuille -> false			
+      else 
+	match d with
+	  | (Noeud(da,b)) ->
+	    let x = String.get s 0 
+	    in let y = (Char.code x) - (Char.code 'a')
+	       in let db =  (Array.get da y)
+		  in member (String.sub s 1 ((String.length s) -1)) db
+	  | Feuille -> false
 
     (*    let (add : string -> dico -> int) = fun s d ->
       let x = String.get s 0
@@ -16,10 +29,32 @@ module Dictionnaire =
   end
 ;;
 
+String.sub "aaaa" 1 3;;
+
+
 let x = Dictionnaire.dico_vide;;
+ 
 
-String.get "salut" 0;; 
+let x =    Dictionnaire.Noeud
+   ([|Dictionnaire.Noeud
+   ([|Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille|],
+   true); Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille; Dictionnaire.Feuille;
+      Dictionnaire.Feuille; Dictionnaire.Feuille|],
+   false);;
 
-add 
-
-let x = (Char.code 'b') - (Char.code 'a');;
+Dictionnaire.member "a" x;; 
